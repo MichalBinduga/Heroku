4530dbaffb382794be29899f5eacd0cb0ba40040
MichalBinduga <m.i.b.i@o2.pl>
2020-04-19 12:26:31 +0200

    Second commit

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -7,6 +7,7 @@ import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
 
 @Component("meetingService")
 public class MeetingService {
@@ -22,5 +23,20 @@ public class MeetingService {
 		Query query = connector.getSession().createQuery(hql);
 		return query.list();
 	}
+	
+	public Collection<Meeting> getAllMeeting() {
+		return connector.getSession().createCriteria(Meeting.class).list();
+	}
+	
+	public Meeting findById(Long Id) {
+		return (Meeting) connector.getSession().get(Meeting.class, Id);
 
+	}
+
+	public Meeting add(Meeting meeting) {
+		Transaction transaction = this.connector.getSession().beginTransaction();
+		connector.getSession().save(meeting);
+		transaction.commit();
+		return meeting;
+	}
 }
